922ff5366122b559ae3d55c9eeaf16707b093d51
Rushion <rushionchegge@gmail.com>
2024-07-18 09:35:14 +0300

    edited scope, met test requirements

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  window.bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = "someone";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "someone else";
+}
